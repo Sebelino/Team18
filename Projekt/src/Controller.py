4ba9372806350ddab8c8f87bb59b4ea9d971a28e
Sebastian Olsson <sebelino@hotmail.com>
2013-05-02 11:42:28 +0200

    Kopplade guit->controller->profilemanager->db.

--- Projekt/src/Controller.py
+++ Projekt/src/Controller.py
@@ -18,17 +18,21 @@ def on_touch_down(self, touch):
         getListOfMappings().[sla upp gesten och returnera kommando]
     '''
     
-
 def on_touch_move(self, touch):
     g = GestureHandler.on_touch_move(touch)
 
 def on_touch_up(self, touch):
     g = GestureHandler.on_touch_up(touch)
 
+#command = ProfileManager.getCommand(gesture)
+#CommandHandler.execute(command)
 
+# For databasen
+def getListOfMappings():
+    return ProfileManager.getMappings()
 
+def getListOfGestures():
+    return ProfileManager.getGestures()
 
-
-
-
-
+def getListOfCommands():
+    return ProfileManager.getCommands()
