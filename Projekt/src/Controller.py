efc30a6fe068dbfe4254910e26ef908993bbf523
Sebelino <sebelino@hotmail.com>
2013-05-02 13:12:47 +0200

    Fixade touch-grejer.

--- Projekt/src/Controller.py
+++ Projekt/src/Controller.py
@@ -9,7 +9,7 @@ def getListOfMappings():
 
 #On touch events
 def on_touch_down(self, touch):
-    g = GestureHandler.on_touch_down(touch)
+    g = GestureHandler.on_touch_down(self,touch)
     #TODO
     '''
     if g == None:
@@ -19,10 +19,10 @@ def on_touch_down(self, touch):
     '''
     
 def on_touch_move(self, touch):
-    g = GestureHandler.on_touch_move(touch)
+    g = GestureHandler.on_touch_move(self,touch)
 
 def on_touch_up(self, touch):
-    g = GestureHandler.on_touch_up(touch)
+    g = GestureHandler.on_touch_up(self,touch)
 
 #command = ProfileManager.getCommand(gesture)
 #CommandHandler.execute(command)
--- Projekt/src/gui.py
+++ Projekt/src/gui.py
@@ -18,7 +18,7 @@ from kivy.graphics import Color,Ellipse,Line
 from kivy.gesture import Gesture,GestureDatabase
 import Gesture as OwnGesture
 from listview import ListView, ScrollViewFixed
-# import Controller
+import Controller
 
 import thread
 import time
@@ -1283,7 +1283,7 @@ class TouchArea(BoxLayout):
         if str(touch.device) == "mouse":
             super(TouchArea, self).on_touch_down(touch)
         elif str(touch.device) == "multitouchtable":
-            Controller.on_touch_down(touch)
+            Controller.on_touch_down(self, touch)
         
 
     def on_touch_move(self, touch):
@@ -1292,7 +1292,7 @@ class TouchArea(BoxLayout):
         if str(touch.device) == "mouse":
             super(TouchArea, self).on_touch_move(touch)
         elif str(touch.device) == "multitouchtable":
-            Controller.on_touch_down(touch)
+            Controller.on_touch_down(self,touch)
         
 
     def on_touch_up(self, touch):
@@ -1301,7 +1301,7 @@ class TouchArea(BoxLayout):
         if str(touch.device) == "mouse":
             super(TouchArea, self).on_touch_up(touch)
         elif str(touch.device) == "multitouchtable":
-            Controller.on_touch_down(touch)
+            Controller.on_touch_down(self,touch)
 
 #and Main
 
